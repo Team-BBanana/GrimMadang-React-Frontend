3d34406b21ec3d95c2c79b0c232c103accaf1826
develsvai <developsvai5096@gmail.com>
2025-01-13 00:53:12 +0000

    feat : 갤러리 API 추가

--- src/api/gallery.api/gallery.api.tsx
+++ src/api/gallery.api/gallery.api.tsx
@@ -1,14 +1,11 @@
 import { CanvasClient } from '..';
 
-const voiceChat = async(data: {transcript: string}) => {
-    const url = `/gallery/voiceChat`;
-    return await CanvasClient.post(url, data, {
-        headers: {
-            "Content-Type": "application/json",
-        },
-        withCredentials: true,
-    });
+
+interface CommentCreateRequest {
+    drawingId: string;
+    content: string;
 }
+
 
 const getElderName = async() => {
     const url = `/gallery/getElderName`;
@@ -17,9 +14,52 @@ const getElderName = async() => {
     });
 }
 
+const getDrawings = async() => {
+    const url = `/api/drawings/getDrawings`;
+    return await CanvasClient.get(url, {
+        withCredentials: true,
+    });
+}
+
+const getDrawing = async(drawingId: string) => {
+    const url = `/api/drawings/${drawingId}`;
+    return await CanvasClient.get(url, {
+        withCredentials: true,
+    });
+}
+
+
+const getFeedbacks = async(drawingId: string) => {
+    const url = `/api/drawings/${drawingId}/getfeedbacks`;
+    return await CanvasClient.get(url, {
+        withCredentials: true,
+    });
+}
+
+const createComment = async(data: CommentCreateRequest) => {
+    const url = `/api/drawings/${data.drawingId}/comment`;
+    return await CanvasClient.post(url, data, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        withCredentials: true,
+    });
+}
+
+const getComments = async(drawingId: string) => {
+    const url = `/api/drawings/${drawingId}/comments`;
+    return await CanvasClient.get(url, {
+        withCredentials: true,
+    });
+}
+
 const galleryApi = {
-    voiceChat,
-    getElderName
+    getElderName,
+    getDrawings,
+    getDrawing,
+    getFeedbacks,
+    createComment,
+    getComments
 };
 
 export default galleryApi;
