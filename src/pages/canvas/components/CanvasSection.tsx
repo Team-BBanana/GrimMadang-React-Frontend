7ace97728d705bb10e163aa0cc98848999ffd570
develsvai <developsvai5096@gmail.com>
2025-01-16 21:53:43 +0000

    Refactor : 캔버스 첫화면 시작하기 버튼 추가

--- src/pages/canvas/components/CanvasSection.tsx
+++ src/pages/canvas/components/CanvasSection.tsx
@@ -488,9 +488,21 @@ const CanvasSection = ({ onUpload, canvasRef, onChange, onFinalSave}: CanvasSect
       onMouseUp={handleMouseUp}
     >
       {!isFirstInteraction && (
-        <div 
+        <div style={{
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          color: 'white',
+          padding: '20px',
+          borderRadius: '10px',
+          zIndex: 1000,
+          cursor: 'pointer'
+        }}
         onClick={handleFirstInteraction}
         >
+          시작하기
         </div>
       )}
       <BannerSection
